ebb4c898450b323eea974ebc2802d05b33245d05
ybw0014 <ybw0014@qq.com>
2018-07-25 10:51:15 -0400

    Initialize

--- src/org/usfirst/frc/team3504/robot/RobotMap.java
+++ src/org/usfirst/frc/team3504/robot/RobotMap.java
@@ -23,4 +23,6 @@ public class RobotMap {
 	// number and the module. For example you with a rangefinder:
 	// public static int rangefinderPort = 1;
 	// public static int rangefinderModule = 1;
+	public static final int DRIVE_MASTER_PORT = 1;
+	public static final int DRIVE_SLAVE_PORT = 1;
 }
