c0eb2a613a495007f12065d7048bc897d6d55a86
ybw0014 <ybw0014@qq.com>
2018-07-30 16:24:47 -0400

    add intake module

--- src/org/usfirst/frc/team3504/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team3504/robot/subsystems/Intake.java
@@ -39,7 +39,6 @@ public class Intake extends Subsystem {
     	rightIntakeMotor.set(-rotationSpeedOfIntake);
     }
     
-    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
