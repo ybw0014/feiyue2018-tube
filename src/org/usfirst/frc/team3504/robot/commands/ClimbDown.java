e9db5bd8896a2e03440d5bd8c574a335f72e78eb
ybw0014 <ybw0014@qq.com>
2018-07-31 11:54:24 -0400

    fix bug in climb cmd

--- src/org/usfirst/frc/team3504/robot/commands/ClimbDown.java
+++ src/org/usfirst/frc/team3504/robot/commands/ClimbDown.java
@@ -23,7 +23,7 @@ public class ClimbDown extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.climber.climb(speed);
+    	Robot.climber.climb(-speed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
