81dfd59f6445d51b48230e92b52cfce003cd41f1
ybw0014 <ybw0014@qq.com>
2018-07-31 11:43:47 -0400

    Create ClimbUp.java

---
+++ src/org/usfirst/frc/team3504/robot/commands/ClimbUp.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team3504.robot.commands;
+
+import org.usfirst.frc.team3504.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ClimbUp extends Command {
+
+	private double speed;
+    public ClimbUp() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.climber);
+    	this.speed = 0.8;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.climber.climb(-speed);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.climber.stopClimb();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
